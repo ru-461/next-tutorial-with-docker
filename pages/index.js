6701a3ad713523d16c8efd050066c43b39a627e6
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-08-15 19:17:19 +0900

    update: Complete Assets, Metadata, and CSS - Metadata

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
-    <h1 className="title">
-      Read <Link href="/posts/first-post">this page!</Link>
-    </h1>
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="title">
+        Read <Link href="/posts/first-post">this page!</Link>
+      </h1>
+    </>
   );
 };
 
