d7f24d8893f9f7780cef5efca170c1b2b0e81ce8
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2022-08-15 20:28:04 +0900

    update: Complete Assets, Metadata, and CSS - Polishing Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// `pages/_app.js`
 import "../styles/global.css";
 
 const App = ({ Component, pagePrpos }) => {
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
-import Link from "next/link";
 import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
-const HomePage = () => {
+const Home = () => {
   return (
-    <>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-      <h1 className="title">
-        Read <Link href="/posts/first-post">this page!</Link>
-      </h1>
-    </>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, everyone.</p>
+        <p>
+          This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
+        </p>
+        <p>
+          The code for this site is available{" "}
+          <a href="https://github.com/ryu-461/next-tutorial-with-docker">
+            here
+          </a>
+          .
+        </p>
+      </section>
+    </Layout>
   );
 };
 
-export default HomePage;
+export default Home;
